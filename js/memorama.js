669762e2936c4e01f1ec8f45694ae4d422f3cbe7
sr-roboto <marcoslopez123.lu@gmail.com>
2024-08-06 20:06:00 -0300

    COMENTE EL CODIGO DE LAS CARTAS

--- js/memorama.js
+++ js/memorama.js
@@ -1,192 +1,70 @@
-var cartas = document.getElementsByTagName("img");
-var puntos = 0;
+let cartas = document.getElementsByTagName("img"); // Selecciona todas las etiquetas <img> (cartas)
+let puntos = 0; // Inicializa la variable para contar los puntos
 
+// Asigna la función `funcionCarta` al evento `onclick` del cuerpo del documento
 document.body.onclick = funcionCarta;
 
-function funcionCarta(){
-    for(var i = 0; i < cartas.length; i++){
-        cartas[i].addEventListener("click", voltear);
-    }
+function funcionCarta() {
+  // Añade el evento `click` a cada carta, asociando la función `voltear`
+  for (let i = 0; i < cartas.length; i++) {
+    cartas[i].addEventListener("click", voltear);
+  }
 }
 
-function voltear(){
+function voltear() {
+  // Obtiene todas las cartas por clase
+  let heart = document.getElementsByClassName("heart");
+  let diamond = document.getElementsByClassName("diamond");
+  let spades = document.getElementsByClassName("spades");
+  let clubs = document.getElementsByClassName("clubs");
 
-var heart = document.getElementsByClassName("heart");
-var diamond = document.getElementsByClassName("diamond");
-var spades = document.getElementsByClassName("spades");
-var clubs = document.getElementsByClassName("clubs");
-
-     //Si las clases coinciden, voltea cada carta
-    for(var x = 0; x < heart.length; x++){
-        if(this == heart[x]){
-            corazon = "corazon";
-            this.src="../assets/memorama/img/heart.png";
-            validarParejas(this, heart, corazon);
-        }
-        if(this == diamond[x]){
-            diamante = "diamante";
-            this.src="../assets/memorama/img/diamond.png";
-            validarParejas(this, diamond, diamante);
-        }
-        if(this == spades[x]){
-            pica = "pica";
-            this.src="../assets/memorama/img/spades.png";
-            validarParejas(this, spades, pica);
-        }
-        if(this == clubs[x]){
-            trebol = "trebol";
-            this.src="../assets/memorama/img/clubs.png";  
-            validarParejas(this, clubs, trebol); 
-        }
+  // Recorre cada conjunto de cartas y compara si la carta actual pertenece a algún conjunto
+  for (let x = 0; x < heart.length; x++) {
+    if (this == heart[x]) {
+      corazon = "corazon";
+      this.src = "../assets/memorama/img/heart.png"; // Cambia la imagen para mostrar el corazón
+      validarParejas(this, heart, corazon); // Valida si se ha formado una pareja
     }
-} 
-
-function validarParejas(esto, palo, clase){
-    if(esto === palo[0]){
-        esto.classList.add(clase);
-    } 
-    if(esto === palo[1]){
-       esto.classList.add(clase);
+    if (this == diamond[x]) {
+      diamante = "diamante";
+      this.src = "../assets/memorama/img/diamond.png"; // Cambia la imagen para mostrar el diamante
+      validarParejas(this, diamond, diamante); // Valida si se ha formado una pareja
     }
-
-    //Verifica que se ha revelado la pareja
-    if(palo[0].matches("." + clase) && palo[1].matches("." + clase)){
-        alert("¡¡Pareja!!");
-        palo[0].style.display="none";
-        palo[1].style.display="none";
-        finPartida();
+    if (this == spades[x]) {
+      pica = "pica";
+      this.src = "../assets/memorama/img/spades.png"; // Cambia la imagen para mostrar la pica
+      validarParejas(this, spades, pica); // Valida si se ha formado una pareja
     }
-    
+    if (this == clubs[x]) {
+      trebol = "trebol";
+      this.src = "../assets/memorama/img/clubs.png"; // Cambia la imagen para mostrar el trébol
+      validarParejas(this, clubs, trebol); // Valida si se ha formado una pareja
+    }
+  }
 }
 
-function finPartida(){
-    puntos++;
-    if(puntos === 4){
-        alert("¡Enhorabuena, has ganado!");
-    }
+function validarParejas(esto, palo, clase) {
+  // Añade la clase correspondiente a la carta seleccionada para identificar su tipo
+  if (esto === palo[0]) {
+    esto.classList.add(clase);
+  }
+  if (esto === palo[1]) {
+    esto.classList.add(clase);
+  }
+
+  // Verifica si ambas cartas del mismo tipo están visibles
+  if (palo[0].matches("." + clase) && palo[1].matches("." + clase)) {
+    alert("¡¡Pareja!!");
+    palo[0].style.display = "none"; // Oculta la primera carta de la pareja
+    palo[1].style.display = "none"; // Oculta la segunda carta de la pareja
+    finPartida(); // Verifica si se ha completado el juego
+  }
 }
 
-// /**
-// *Array asociativo con cartas. El atributo dibujo es indice
-// *
-// */
-// const arrayCartas = [
-//     {
-//         dibujo: 'Daredevil',
-//         imagen: '/LEXIMATE-FRONT/assets/memorama/img/daredevil.jpg'
-//     },
-//     {
-//         dibujo: 'Spiderman',
-//         imagen: '/LEXIMATE-FRONT/assets/memorama/img/spiderman.jpg'
-//     },
-//     {
-//         dibujo: 'Hulk',
-//         imagen: '/LEXIMATE-FRONT/assets/memorama/img/hulk.png'
-//     },
-//     {
-//         dibujo: 'Thor',
-//         imagen: '/LEXIMATE-FRONT/assets/memorama/img/thor.jpg'
-//     },
-//     {
-//         dibujo: 'Thor',
-//         imagen: '/LEXIMATE-FRONT/assets/memorama/img/thor.jpg'
-//     }
-//     ,
-//     {
-//         dibujo: 'Spiderman',
-//         imagen: '/LEXIMATE-FRONT/assets/memorama/img/spiderman.jpg'
-//     },
-//     {
-//         dibujo: 'Hulk',
-//         imagen: '/LEXIMATE-FRONT/assets/memorama/img/hulk.png'
-//     },
-//     {
-//         dibujo: 'Daredevil',
-//         imagen: '/LEXIMATE-FRONT/assets/memorama/img/daredevil.jpg'
-//     }
-// ]
-
-// //variables globales
-// //Para manejar cuantas tengo seleccionadas en el momento
-// var seleccionadas = 0;
-// var cartaAnterior = -1;
-// var movimientos = 0;
-// var aciertos = 0;
-
-
-// /**
-// *Randomicamante asigna posiciones a las cartas
-// *
-// *Dentro del mismo array declarado anteriormente de le asignan posiciones 
-// *randomica a las mismas (que luego estarán representadas por los números  de celdas 
-// *declarados en HTML).
-// *
-// *Le asigna a cada celda en la tabla HTML un estilo que tiene como imagen de fondo el reverso de 
-// *la carta - Ver en estilos
-// */
-// function mezclarCartas(){
-//      arrayCartas.sort(() => Math.random() - 0.5);
-//      //console.log(arrayCartas);
-//      for (var i =0; i < arrayCartas.length ;i++) {
-//          document.getElementById(i).setAttribute("class", "carta cartaReverso");
-//      }
-// }
-
-
-// /**
-// *Muestra la imagen de la carta
-// *
-// *Cuando el usuario selecciona la carta (celda en la tabla HTML), cambiamos el estilo de la 
-// *celda poniendo como imagen de fondo, la correspondiente del array de cartas
-// *
-// */
-// function mostrarCarta(i){
-//     document.getElementById(i).style.backgroundImage = "url('"+arrayCartas[i].imagen+"')";
-
-// }
-
-
-// /**
-// *La carta se oculta mostrando el reverso
-// *
-// *Cuando no hay coincidencia, las cartas vuelvne a mostrar el reverso
-// *Se le asigna el fondo a la celda de el reverso
-// *
-// */
-// function ocultarCarta(i){
-//     document.getElementById(i).style.backgroundImage = "url('/LEXIMATE-FRONT/assets/memorama/img/cartaReverso.png')";
-// }
-
-
-// /**
-// *Selecciona carta y analiza jugada
-// *
-// *Cuando el usuario selecciona una carta, debera dar veulta otro. El algoritmo verifica cual es, 
-// * y a partir de eso toma la desicion de si es un "MATCH" o al medio segundo las vuelve 
-// *a su sposicion original
-// *
-// */
-// function elegirCarta(i){
-//     mostrarCarta(i);
-//     if (seleccionadas == 0){
-//         cartaAnterior = i;
-//         seleccionadas = 1;	
-//     }else{
-//         if(arrayCartas[cartaAnterior].dibujo === arrayCartas[i].dibujo){
-//             aciertos++;
-//         }else{
-//             setTimeout(() => {  ocultarCarta(cartaAnterior); ocultarCarta(i); }, 500);
-//         }
-//     seleccionadas = 0;
-//     movimientos++;	
-//     }
-    
-//     document.getElementById("movimientos").innerHTML = movimientos;
-
-//     if (aciertos == 4){
-//         document.getElementById("ganador").innerHTML = "GANASTE!!!!";
-//     }
-
-// }
-
-
+function finPartida() {
+  puntos++; // Incrementa los puntos por cada pareja encontrada
+  if (puntos === 4) {
+    // Verifica si se han encontrado todas las parejas (asumiendo 4 parejas)
+    alert("¡Enhorabuena, has ganado!");
+  }
+}
